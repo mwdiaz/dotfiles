2ab679def58ef7b7308532635b958446c420dcbe
Mike Diaz <michaelwdiaz@gmail.com>
2026-05-06 09:13:50 -0700

    Add constants for Oh My Zsh

---
+++ home/dot_config/oh-my-zsh/constants.zsh
@@ -0,0 +1,13 @@
+# Prefer U.S. English and UTF-8 (https://www.gnu.org/software/gettext/manual/html_node/Locale-Environment-Variables.html)
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
+export EDITOR="vim"
+export VISUAL="code --wait"
+
+export FORCE_COLOR=1 # https://force-color.org
+
+# Disable Open Collective postinstall scripts
+# (https://github.com/opencollective/opencollective-postinstall)
+export DISABLE_OPENCOLLECTIVE=1
