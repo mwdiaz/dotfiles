dcd4a7a54e83d7e6cd28eb5112c60a9bcbbf0fa6
Mike Diaz <michaelwdiaz@gmail.com>
2026-05-21 00:11:25 -0700

    Remove unused script

--- .install-password-manager.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-. "$HOME"/.local/share/chezmoi/utils.sh
-
-# Check if 'op' command already exists
-if command -v op >/dev/null 2>&1; then
-  echo "op command already exists"
-  exit 0
-fi
